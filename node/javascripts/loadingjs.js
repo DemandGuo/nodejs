3af1217d2fdf7158ac3dc93d8021fbc6a1e8e63b
gyz <1172792033@qq.com>
2025-12-22 22:10:08 +0800

    feat: 添加 importScript 函数，支持动态加载脚本

---
+++ node/javascripts/loadingjs.js
@@ -0,0 +1,9 @@
+function importScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => resolve();
+        script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+        document.head.appendChild(script);
+    });
+}
